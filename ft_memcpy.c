82baf96905210721ae6b8239ddee71417b761813
Kimy Lucchini <lucchini.kimy@gmail.com>
2025-11-04 17:12:52 +0100

    more more

---
+++ ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klucchin <klucchin@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 21:43:33 by klucchin          #+#    #+#             */
+/*   Updated: 2025/11/04 17:11:38 by klucchin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memcpy(void *dest, const void *src, unsigned int n)
+{
+	unsigned char	*ptr;
+	unsigned int	i;
+
+	src = (unsigned char *)src;
+	dest = (unsigned char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klucchin <klucchin@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/03 21:27:13 by klucchin          #+#    #+#             */
+/*   Updated: 2025/11/04 17:07:07 by klucchin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memset(void *s, int c, unsigned int n)
+{
+	unsigned char	*ptr;
+	unsigned int	i;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
